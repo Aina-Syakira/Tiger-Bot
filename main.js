cdc41dac06b2f351495718e3a831ffd63f7900a7
Aina-Syakira <34301494+Aina-Syakira@users.noreply.github.com>
2018-12-19 10:01:34 +0800

    Update main.js

--- main.js
+++ main.js
@@ -747,7 +747,7 @@ client.on('message', async message=>{
             var embed = new Discord.RichEmbed()
               .setAuthor('Server Suggestion', client.user.avatarURL)
               .addField('Suggestion', args.join(' '))
-              .addField('Suggested By','**Nama**:' `${message.author.username}`)
+              .addField('Suggested By', `${message.author.username}`)
 	            .setColor(0x1D82B6)
 	            .setTimestamp()
         let msg = await message.channel.send(embed);
