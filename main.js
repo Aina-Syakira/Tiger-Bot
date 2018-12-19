09611519a3c8133daa9b2d9d0100df12d024789e
Aina-Syakira <34301494+Aina-Syakira@users.noreply.github.com>
2018-12-19 10:04:20 +0800

    Update main.js

--- main.js
+++ main.js
@@ -748,6 +748,7 @@ client.on('message', async message=>{
               .setAuthor('Server Suggestion', client.user.avatarURL)
               .addField('Suggestion', args.join(' '))
               .addField('Suggested By', `${message.author.username}`)
+              .setFooter('Username')
 	            .setColor(0x1D82B6)
 	            .setTimestamp()
         let msg = await message.channel.send(embed);
